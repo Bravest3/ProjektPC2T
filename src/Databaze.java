62f6072e9c01abc92d6af98cda80b7e360ca9804
marie484 <mariep03@seznam.cz>
2026-05-05 14:13:59 +0200

    jeszte to chce ukoly a databazi

--- src/Databaze.java
+++ src/Databaze.java
@@ -6,6 +6,14 @@ import java.util.random.Random;
 public class Databaze {
 
     public HashMap<Integer, ZamestnanecBase> seznamZamestnancu = new HashMap<>();
+
+    public enum UrovenSpoluprace {
+        NIZKA,
+        STREDNI,
+        VYSOKA
+    }
+
+    public HashMap<Integer, HashMap<Integer, UrovenSpoluprace>> spoluprace = new HashMap<>();
     int citac = 1;
     //spoluprace
 
@@ -15,6 +23,14 @@ public class Databaze {
 
     public HashMap<Integer, ZamestnanecBase> getSeznamZamestnancu() {
         return seznamZamestnancu;
+    }
+
+    public void setSpoluprace(HashMap<Integer, HashMap<Integer, UrovenSpoluprace>> spoluprace) {
+        this.spoluprace = spoluprace;
+    }
+
+    public HashMap<Integer, HashMap<Integer, UrovenSpoluprace>> getSpoluprace() {
+        return spoluprace;
     }
 
     public void pridejZamestnance(ZamestnanecBase zamestnanec) {
@@ -51,6 +67,8 @@ public class Databaze {
                 System.out.println("Neplatná volba zaměstnance. Zadejte znovu.");
                 continue;
             }
+
+            spoluprace.put(id, new HashMap<>());
         }
     
     
@@ -63,33 +81,97 @@ public class Databaze {
 
     public void inicializace() {
         seznamZamestnancu.put(citac, new DatovyAnalytik(citac, 1990, "Jan", "Novak"));
+        spoluprace.put(citac, new HashMap<>());
         citac++;
         seznamZamestnancu.put(citac, new BezpectnostniSpecialista(citac, 1985, "Petr", "Svoboda"));
+        spoluprace.put(citac, new HashMap<>());
         citac++;
         seznamZamestnancu.put(citac, new DatovyAnalytik(citac, 1992, "Eva", "Kralova"));
+        spoluprace.put(citac, new HashMap<>());
         citac++;
         seznamZamestnancu.put(citac, new BezpectnostniSpecialista(citac, 1988, "Martin", "Dvorak"));
+        spoluprace.put(citac, new HashMap<>());
         citac++;
         seznamZamestnancu.put(citac, new DatovyAnalytik(citac, 1995, "Lucie", "Horakova"));
+        spoluprace.put(citac, new HashMap<>());
+
         citac++;
+        novaSpoluprace(1, 2, UrovenSpoluprace.VYSOKA);
+        novaSpoluprace(1, 3, UrovenSpoluprace.NIZKA);
+        novaSpoluprace(2, 4, UrovenSpoluprace.STREDNI);
+        novaSpoluprace(3, 5, UrovenSpoluprace.VYSOKA);
+
         System.out.println("Přidáno 5 zaměstnanců do databáze.");
     }
 
-    public void vypisZamestnance() {
-        System.out.println("Zadejte ID: ");
-        Scanner sc = new Scanner(System.in);
-        int id = sc.nextInt();
+    public void vypisZamestnance(int id) {
+        
         if (seznamZamestnancu.containsKey(id)) {
             ZamestnanecBase zamestnanec = seznamZamestnancu.get(id);
             System.out.println("ID: " + zamestnanec.getID());
             System.out.println("Jméno: " + zamestnanec.getJmeno());
             System.out.println("Příjmení: " + zamestnanec.getPrijmeni());
             System.out.println("Rok narození: " + zamestnanec.getRokNarozeni());
+            vypisSpoluprace(id);
         }
         else {
             System.out.println("Zaměstnanec s ID " + id + " nenalezen.");
         }
     }
 
+    public void vypis(boolean analytik) {
+        for (Map.Entry<Integer, ZamestnanecBase> entry : seznamZamestnancu.entrySet()) {
+            if (analytik && entry.getValue() instanceof DatovyAnalytik) {
+                vypisZamestnance(entry.getValue().getID());
+            }
+            else if (!analytik && entry.getValue() instanceof BezpectnostniSpecialista) {
+                vypisZamestnance(entry.getValue().getID());
+            }
+        }
+    }
 
+    public void odeberZamestnance(int id) {
+        if (seznamZamestnancu.containsKey(id)) {
+            seznamZamestnancu.remove(id);
+            spoluprace.remove(id);
+
+            for (HashMap<Integer, UrovenSpoluprace> spolupraceZamestnance : spoluprace.values()) {
+                spolupraceZamestnance.remove(id);
+            }
+
+            System.out.println("Zaměstnanec s ID " + id + " byl odebrán.");
+        }
+        else {
+            System.out.println("Zaměstnanec s ID " + id + " nenalezen.");
+        }
+    }
+
+    public void novaSpoluprace(int id1, int id2, UrovenSpoluprace uroven) {
+        if (seznamZamestnancu.containsKey(id1) && seznamZamestnancu.containsKey(id2)) {
+            spoluprace.get(id1).put(id2, uroven);
+            spoluprace.get(id2).put(id1, uroven);
+            System.out.println("Spolupráce mezi zaměstnanci " + id1 + " a " + id2 + " byla nastavena na úroveň: " + uroven);
+        }
+        else {
+            System.out.println("Jeden nebo oba zaměstnanci nebyli nalezeni.");
+        }
+    }
+
+    public void vypisSpoluprace(int id) {
+        if (seznamZamestnancu.containsKey(id)) {
+            System.out.println("Spolupráce:");
+            HashMap<Integer, UrovenSpoluprace> spolupraceZamestnance = spoluprace.get(id);
+            for (Map.Entry<Integer, UrovenSpoluprace> entry : spolupraceZamestnance.entrySet()) {
+                System.out.println("Spolupráce s" + entry.getKey() + ": " + entry.getValue());
+            }
+            System.out.println("\n");
+        }
+        else {
+            System.out.println("Zaměstnanec s ID " + id + " nenalezen.");
+        }
+    }
+
+    public void pocetZamestnancu() {
+        System.out.println("Počet zaměstnanců: " + seznamZamestnancu.size());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,7 @@ public class Main {
         
 
         while (true) {
-            //tady bude hlavni menu
-            System.out.println("Zadejte 0 pro ukončení programu, 1 pro přidání zaměstnance, 11 pro inicializaci databáze:");
+            System.out.println("\nZadejte 0 pro ukončení programu\n1 pro přidání zaměstnance\n2 pro zobrazení zaměstnance\n3 pro zobrazení zaměstnanců podle role\n11 pro inicializaci databáze:");
             String volba = volbaSc.nextLine();
             if (volba.equals("0")) {
                 System.out.println("Ukončuji program...");
@@ -24,8 +23,47 @@ public class Main {
             else if (volba.equals("1")) {
                 dat.pridejZamestnance(null);
             }
-            else if (volba.equals("2")) {
-                dat.vypisZamestnance();
+            else if (volba.equals("2")) { //pridani spoluprace
+                int id1, id2, sp;
+                System.out.println("Zadejte ID prvního zaměstnance: ");
+                id1 = volbaSc.nextInt();
+                System.out.println("Zadejte ID druhého zaměstnance: ");
+                id2 = volbaSc.nextInt();
+                System.out.println("Zvolte úroveň spolupráce (1 - nízká, 2 - střední, 3 - vysoká): ");
+                sp = volbaSc.nextInt();
+                Databaze.UrovenSpoluprace uroven = null;
+                switch (sp) {   
+                    case 1 -> uroven = Databaze.UrovenSpoluprace.NIZKA;
+                    case 2 -> uroven = Databaze.UrovenSpoluprace.STREDNI;
+                    case 3 -> uroven = Databaze.UrovenSpoluprace.VYSOKA;
+                    default -> System.out.println("Neplatná volba úrovně spolupráce.");
+                }
+                dat.novaSpoluprace(id1, id2, uroven);
+            }
+            else if (volba.equals("3")) { //odebrani
+                System.out.println("Zadejte ID zaměstnance, kterého chcete smazat: ");
+                int id = volbaSc.nextInt();
+                dat.odeberZamestnance(id);
+            }
+            else if (volba.equals("4")) { //vyhledani                              
+                System.out.println("Zadejte ID: ");
+                int id = volbaSc.nextInt();
+                dat.vypisZamestnance(id);
+            }
+            else if (volba.equals("5")) { //spusteni dovednosti
+                
+            }  
+            else if (volba.equals("6")) { //vypis,
+                System.out.println("Zvolte 1 pro zobrazení datových analytiků, 2 pro zobrazení bezpečnostních specialistů: ");
+                int volbaAnalytik = volbaSc.nextInt();
+                dat.vypis(volbaAnalytik == 1);
+            }
+            else if (volba.equals("7")) { //statistika, 
+                }
+            else if (volba.equals("8")) {//vypis poctu
+                dat.pocetZamestnancu();
+                }
+            else if (volba.equals("9")) { //
             }
             else if (volba.equals("11")) {
                 dat.inicializace();
